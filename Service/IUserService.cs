1265de0e49af5787084ad1de6fa29aebbc9f94ac
Gitesh <giteshsingh17@gmail.com>
2021-04-03 12:56:41 +0530

    Add project files.

---
+++ Service/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using microservice.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace microservice.Service
+{
+   public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
